a7e92762bfa608c85b814d9991c715de92546201
krish <krish@localhost>
2026-02-04 09:07:46 -0800

    Implemented constructors + isEmpty + size methods

--- TravelingSalesman/src/Tour.java
+++ TravelingSalesman/src/Tour.java
@@ -20,25 +20,42 @@ public class Tour {
     
     
     public Tour() {
-    	
+    	home = null;
+    	size = 0;
     }
 
     
     public Tour(Point a, Point b, Point c, Point d) {
+    	Node n1 = new Node();
+    	Node n2 = new Node();
+    	Node n3 = new Node();
+    	Node n4 = new Node();
     	
+    	n1.data = a;
+    	n2.data = b;
+    	n3.data = c;
+    	n4.data = d;
+    	
+    	n1.next = n2;
+    	n2.next = n3;
+    	n3.next = n4;
+    	n4.next = n1;
+    	
+    	home = n1;
+    	size = 4;
     }
     
     public boolean isEmpty() {
-    	return false;
+    	return size == 0;
     }
     
     public int size() {
-        return 0;
+    	return size;
     }
 
     
     public double length() {
-        return 0;
+        
     }
 
     
